013e1017575a3bd1a64d6a6f5b21631034810bfb
Tare <112823522+tarik00000@users.noreply.github.com>
2024-07-25 17:23:58 +0200

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,3 +1,4 @@
+# Napiši program koji unosi string, a zatim provjerava da li je palindrom, tj. da li se čita isto sa obje strane. 
 string = input("Unesi string: ")
 if string == string[::-1]:
     print("String je palindrom.")
